54981e75817077635ad36e6b4a61fd4dae74d61d
min-0 <dhe08133@naver.com>
2021-07-31 03:44:34 +0900

    Home View

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 #from django.http import HttpResponse
 #from datetime import datetime
+from math import ceil
 from django.shortcuts import render
 from . import models
 
@@ -11,8 +12,24 @@ def all_rooms(request):
     # hungry = True
     # return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
 
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", context={"rooms":all_rooms})
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit] #offset and limit
+    page_count = models.Room.objects.count()/page_size
+    page_count = ceil(models.Room.objects.count() / page_size)
+    return render(
+        request,
+        "rooms/home.html",
+        {
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count),
+        },
+    )
 
 
 """ html 파일에서 변수를 보여주고 싶을 땐 {{변수명}}
