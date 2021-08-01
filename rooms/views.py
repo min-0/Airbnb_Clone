02fc7e8b64d371eff543597b11809fcad49707f6
min-0 <dhe08133@naver.com>
2021-08-01 19:06:07 +0900

    room_detail FBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,10 @@
 #from django.http import HttpResponse
 #from datetime import datetime, timezone
-from django.core import paginator
+from django.core import exceptions, paginator
 from django.shortcuts import redirect, render
 from django.core.paginator import EmptyPage, Paginator
 from django.views.generic import ListView
+from django.urls import reverse
 from . import models
 
 """
@@ -49,6 +50,12 @@ class HomeView(ListView):
         context["now"] = now
         return context"""
     
-    def room_detail(request, pk):
-        print(pk)
-        return render(request, "rooms/detail.html")
+    
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room":room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
+
+
